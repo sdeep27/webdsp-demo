b1707e9af848798c244f73b198de7057f88b59e7
Deep Pulusani <sdeep27@yahoo.com>
2017-03-25 14:10:29 -0700

    testing styled components and material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import styled from 'styled-components';
+
+//let ppl know if they're browser is working with WebAssembly
+
+const Intro = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Intro>Welcome to WEBDSP homies</Intro>
+      </MuiThemeProvider>
     );
   }
 }
